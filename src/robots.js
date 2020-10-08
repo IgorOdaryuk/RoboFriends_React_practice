de7dc6ffc70a446a003ea16880ac688cd2addfe2
Igor_Odaryuk <odaryukigor@gmail.com>
2020-10-08 16:59:45 +0300

    first commit

---
+++ src/robots.js
@@ -0,0 +1,62 @@
+export const robots = [
+  {
+    id: 1,
+    name: "John Lennon",
+    username: "John",
+    email: "JohnLennon@gmail.com",
+  },
+  {
+    id: 2,
+    name: "Paul McCartney",
+    username: "Paul",
+    email: "PaulMcCartney@gmail.com",
+  },
+  {
+    id: 3,
+    name: "George Harrison",
+    username: "George",
+    email: "George Harrison@gmail.com",
+  },
+  {
+    id: 4,
+    name: "Ringo Starr",
+    username: "Ringo",
+    email: "RingoStarr@gmail.com",
+  },
+  {
+    id: 5,
+    name: "Pete Best",
+    username: "Pete",
+    email: "PeteBest@gmail.com",
+  },
+  {
+    id: 6,
+    name: "Bryan Epstein",
+    username: "Bryan",
+    email: "BryanEpstein@gmail.com",
+  },
+  {
+    id: 7,
+    name: "Maharishi",
+    username: "Guru",
+    email: "GuruIndian@gmail.com",
+  },
+  {
+    id: 8,
+    name: "The Walrus",
+    username: "Walrus",
+    email: "iamthewalrus@gmail.com",
+  },
+  {
+    id: 9,
+    name: "George Martin",
+    username: "GeorgeMartin",
+    email: "GeorgeMartin@gmail.com",
+  },
+  {
+    id: 10,
+    name: "Billy Preston",
+    username: "Billy",
+    email: "BillePreston@gmail.com",
+  },
+];
